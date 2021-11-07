f1d1b3bb5d7c34fe787ce4da77774f917afa6eb7
Alieze-Ali <71855821+Alieze-Ali@users.noreply.github.com>
2021-11-07 10:29:23 -0800

    basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import PrivateRoute from './PrivateRoute';
 import styled from 'styled-components';
 
@@ -11,25 +11,35 @@ import Logout from './Logout';
 
 const App = () => {
   return (
-    <AppContainer>
-      <LambdaHeader/>
-      <Header/>
-      <RouteContainer>
-        <Route exact path="/">
-          <Login/>
-        </Route>          
-      </RouteContainer>
-    </AppContainer>
+    // Basic Routing
+    <Router>
+
+        <AppContainer>
+          <LambdaHeader/>
+            <Header/>
+              <RouteContainer>
+              
+                <Route path="/login" component={Login} />
+                <Route exact path="/" component={Login} />
+                  
+                  {/* </Route>      */}
+                <PrivateRoute path="/view" component={View} />
+                <PrivateRoute path="/logout" component={Logout}/>     
+              </RouteContainer>
+        </AppContainer>
+
+    </Router>
+    
   )
 }
 
 export default App;
 
 //Task List
-//1. Create and import PrivateRoute component.
-//2. Create a Route for Login pointing to '/login.'
-//3. Create a PrivateRoute for View component point to '/view.'
-//4. Create a PrivateRoute for Logout component pointing to '/logout.'
+//1. Create and import PrivateRoute component. ok I think???
+//2. Create a Route for Login pointing to '/login.' - ok
+//3. Create a PrivateRoute for View component point to '/view.' ok
+//4. Create a PrivateRoute for Logout component pointing to '/logout.'-ok
 
 
 const AppContainer = styled.div`
